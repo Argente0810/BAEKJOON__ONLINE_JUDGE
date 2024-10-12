5e3dbeaad6f8b1f9871f3c3f4cdac2dd8cfb5d49
Johan <ra291345@daum.net>
2024-10-12 00:03:40 -0600

    Update 2166 Area of ​​a polygon.cpp

--- Geometry2/2166 Area of ​​a polygon.cpp
+++ Geometry2/2166 Area of ​​a polygon.cpp
@@ -1,7 +1,33 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-int main(){
-  
-  
+struct Point {
+    double x, y;
+};
+
+int main() {
+    
+    int n;
+    cin >> n;
+
+    vector<Point> points(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> points[i].x >> points[i].y;
+    }
+
+    double area = 0.0;
+    for (int i = 0; i < n; i++) {
+        int next = (i + 1) % n;
+        area += points[i].x * points[next].y - points[next].x * points[i].y;
+    }
+
+    area = abs(area) / 2.0;
+
+    cout << fixed;
+    cout.precision(1);
+    cout << area << endl;
+
 }
