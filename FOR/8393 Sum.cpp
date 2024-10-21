e81ab19db00d4739889bba36e1f8c753348a1a03
Johan <ra291345@daum.net>
2024-10-21 14:33:04 -0700

    Create 8393 Sum.cpp

---
+++ FOR/8393 Sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int p,n = 0;
+    
+    cin >> p;
+    
+    for(int i = 0; i <= p; i++){
+        n += i;
+    }
+    cout << n;
+    
+}
