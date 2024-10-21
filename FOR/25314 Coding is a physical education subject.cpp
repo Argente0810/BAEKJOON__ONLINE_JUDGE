55a259b52bbaf1502c13b3b94cd4dc3d4e4abd05
Johan <ra291345@daum.net>
2024-10-21 14:45:14 -0700

    Create 25314 Coding is a physical education subject.cpp

---
+++ FOR/25314 Coding is a physical education subject.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n, m;
+    
+    cin >> n;
+    m = n / 4;
+    
+    for(int i = 0; i < m; i++){
+        cout << "long ";
+    }
+    cout << "int" << endl;
+}
