deefb0134d2edbfb0dea8b9a641483974e2a8417
Johan <ra291345@daum.net>
2024-11-06 22:09:37 -0800

    Create 21293 No Description 2.cpp

---
+++ 구데기컵/21293 No Description 2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int z, y, x, w, v, u, t, s, r, p, o, n, m, l, k, j, i, h, g, f, e, d, c, b, a;
+
+
+    cin >> z >> y >> x >> w >> v >> u >> t >> s >> r >> p >> o >> n >> m >> l >> k >> j
+        >> i >> h >> g >> f >> e >> d >> c >> b >> a;
+
+    cout << a - b << endl;
+
+    cout << a * b << endl;
+
+    cout << a + b + 1 << endl;
+
+    return 0;
+}
